011541753ba4584aac1061d722983ceb8eb806b8
Dwesh163 <ehouarn.duriaux@protonmail.com>
2023-11-08 17:23:54 +0100

    [update] send error

--- scripts/verifyDocker.py
+++ scripts/verifyDocker.py
@@ -6,12 +6,13 @@ import json
 
 load_dotenv()
 
-dockerReturn = subprocess.getoutput("docker ps")
+dockerReturn = subprocess.getoutput("docker ps -a")
 dockerReturnList = dockerReturn.split("\n")
 dockerReturnTitle = dockerReturnList[0].split("   ")
 
 dockerReturnData = {}
 dockerReturnNewTitle = []
+dockerReturnErrorData = []
 
 for i in range(len(dockerReturnTitle)):
     if dockerReturnTitle[i] != "":
@@ -30,19 +31,29 @@ for i in range(1, len(dockerReturnList)):
         dockerReturnNewTitle[1] : dockerReturnNewList[1],
         dockerReturnNewTitle[2] : dockerReturnNewList[2],
         dockerReturnNewTitle[3] : dockerReturnNewList[3],
-        dockerReturnNewTitle[4] : dockerReturnNewList[4],
-        dockerReturnNewTitle[5] : dockerReturnNewList[5],
-        dockerReturnNewTitle[6] : dockerReturnNewList[6],
-
+        dockerReturnNewTitle[4] : dockerReturnNewList[4]
     }
 
-response = f"Status : OK 4/{len(dockerReturnData)}\n  STATUS              NAMES\n"
+    if len(dockerReturnData) == 5:
+        dockerReturnData[dockerReturnNewList[0]][dockerReturnNewTitle[6]] = dockerReturnNewList[5]
+        dockerReturnData[dockerReturnNewList[0]][dockerReturnNewTitle[5]] = dockerReturnNewList[6]
+    else:
+        dockerReturnData[dockerReturnNewList[0]][dockerReturnNewTitle[6]] = dockerReturnNewList[5]
 
-for container in dockerReturnData:
-    response += f"  {dockerReturnData[container]['STATUS']} {dockerReturnData[container]['NAMES']}\n"
+    if "Exited" in dockerReturnData[dockerReturnNewList[0]]["STATUS"]:
+        dockerReturnErrorData.append(dockerReturnNewList[0])
 
 
-def main(response):
+
+response = f"Status : OK 4/{len(dockerReturnData)}"
+error = "  STATUS              NAMES\n"
+
+for container in dockerReturnErrorData:
+    print(dockerReturnData[container]['STATUS'])
+    error += f"  {dockerReturnData[container]['STATUS']} {dockerReturnData[container]['NAMES']}\n"
+
+
+def main(response, error):
 
     updater = Updater(os.getenv("BOT_CONFIG_TOKEN"), use_context=True)
 
@@ -50,9 +61,10 @@ def main(response):
     bot = updater.bot
 
     bot.send_message(chat_id=os.getenv("ADMIN_TELEGRAM_ID"), text=response)
+    bot.send_message(chat_id=os.getenv("ADMIN_TELEGRAM_ID"), text=error)
 
     updater.start_polling()
 
 
 if __name__ == '__main__':
-    main(response)
+    main(response, error)
